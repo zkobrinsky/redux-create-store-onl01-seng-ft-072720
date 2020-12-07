d1c72189655a40a66941a8a175127d5a0a35f40a
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-12-07 14:56:30 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,21 @@
-let state;
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +27,23 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+
+
+
+const store = createStore(reducer)
+// console.log(dispatch)
+store.dispatch({ type: '@@INIT' })
+
+
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
